e35cd9eae941728470210d9e190bf56d9ab7a3d3
Mia Dinh <miadinh38@gmail.com>
2023-08-04 23:40:44 -0400

    Practice class-basec OOP

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
 
 }
 
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+  
+  get value() {
+    return this.amount;
+  }
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("billybob");
+
+console.log("Stating Balance:", myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount)
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log("Ending Balance", myAccount.balance);
